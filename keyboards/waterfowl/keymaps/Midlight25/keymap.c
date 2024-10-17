1419ee803607eec2b39b22128b13755a78f79e7b
Midlight25 <mannolaur@gmail.com>
2024-10-16 22:07:23 -0500

    Modified SYM, NUM, and SHORTS
    Modifed symbol, numbers, and some of the shortcut layer.

--- keyboards/waterfowl/keymaps/Midlight25/keymap.c
+++ keyboards/waterfowl/keymaps/Midlight25/keymap.c
@@ -98,10 +98,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Homerow Mods enabled
  */
 [_SYM] = LAYOUT(
-	KC_PERC,	KC_AT,			KC_LBRC,		KC_RBRC,	KC_BSLS,		KC_NO,		KC_NO,		KC_CIRC,	KC_NO,			KC_NO,
-	KC_HASH,	KC_EXLM,		KC_LPRN,		KC_RPRN,	KC_PIPE,		KC_UNDS,	KC_QUOT,	KC_DQT,		KC_TILD,		KC_GRV,
-	KC_DLR,		KC_NO,			KC_LCBR,		KC_RCBR,	KC_AMPR,		KC_NO,		KC_NO,		KC_NO,		KC_NO,			KC_NO,
-	KC_NO,		KC_NO,			KC_NO,		    KC_NO,		KC_NO,		    KC_TRNS,	KC_NO,		KC_TRNS,	KC_NO,	        KC_NO
+	KC_EXLM,	KC_HASH,		KC_LBRC,		KC_RBRC,	KC_QUOT,		KC_GRV,		KC_BSLS,	KC_ASTR,	KC_NO,			KC_NO,
+	KC_AT,  	KC_DLR, 		KC_LPRN,		KC_RPRN,	KC_AMPR,		KC_TILD,	KC_EQL,	    KC_PLUS,	KC_MINS,		KC_UNDS,
+	KC_CIRC,	KC_PERC,		KC_LCBR,		KC_RCBR,	KC_PIPE,		KC_NO,		KC_DQT,		KC_NO,		KC_NO,			KC_NO,
+	KC_NO,		KC_NO,			KC_NO,		    KC_NO,		KC_NO,		    KC_TRNS,	KC_NO,		KC_NO,	    KC_NO,	        KC_NO
 ),
 
 
@@ -120,19 +120,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Homerow Mods enabled
  */
 [_NUM] = LAYOUT(
-	KC_NO,		KC_PGUP,		KC_UP,			KC_PGDN,	KC_NO,				KC_SLSH,	KC_7,	KC_8,		KC_9,			KC_MINS,
-	KC_HOME,	KC_LEFT,		KC_DOWN,		KC_RGHT,	KC_END,				KC_EQL,		KC_4,	KC_5,		KC_6,			KC_PLUS,
-	KC_NO,		KC_NO,			KC_INS,			KC_NO,		KC_NO,				KC_0,		KC_1,	KC_2,		KC_3,			KC_ASTR,
-	KC_NO,		KC_NO,			KC_NO,		    KC_TRNS,	KC_NO,		        KC_TRNS,	KC_NO,	KC_TRNS,	KC_NO,	        KC_NO
+	KC_F1,		KC_F2,			KC_F3,			KC_F4,		KC_F5,		KC_F6,		KC_F7,			KC_F8,			KC_F9,		KC_F10,
+	KC_1,		KC_2,			KC_3,			KC_4,		KC_5,		KC_6,		KC_7,			KC_8,			KC_9,		KC_0,
+	KC_F11,		KC_F12,			KC_F13,			KC_F15,		KC_NO,		KC_NO,		KC_F17,			KC_NO,			KC_NO,		KC_NO,
+	KC_NO,		KC_NO,			KC_NO,			KC_TRNS,	KC_NO,		KC_NO,	    KC_NO,	        KC_NO,	        KC_NO,	    KC_NO
 ),
-
 
 /* Shortcuts
  *
  * ,----------------------------------.                      ,----------------------------------.
- * |      |      |Expand|      | Help |                      | Save | S+INS| Info |Detail|Search|
+ * | Clear|      |Expand|      | Help |                      | Save | S+INS| Info |Detail|Search|
  * |------+------+------+------+------|                      |------+------+------+------+------|
- * |      | Scrn |  DEL | BKSP |      |                      | Back | Play | Next |      |      |
+ * |      | Scrn |  DEL | BKSP |      |                      | Prev | Play | Next |      |      |
  * |------+------+------+------+------|  ,-----.    ,-----.  |------+------+------+------+------|
  * |      | ShrX | CAPW | Menu |      |  |     |    |     |  | Lock |      |      |      |      |
  * `----------------------------------'  `-----'    `-----'  `----------------------------------'
@@ -140,11 +139,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *          |     |   |     |   ^   |      |            |     |       |      |   |     |
  *          `-----'   `--------------------'            `--------------------'   `-----'
 */
-[_SHORT] = LAYOUT(
-	KC_NO,	KC_NO,			KC_NO,			KC_NO,	KC_NO,			KC_NO,	    KC_F7,	KC_F8,		KC_F9,			KC_F11,
-	KC_NO,	KC_NO,			KC_NO,			KC_NO,	KC_NO,			KC_NO,	    KC_F4,	KC_F5,		KC_F6,			KC_F12,
-	KC_NO,	KC_NO,			KC_NO,			KC_NO,	KC_NO,			KC_F10,	    KC_F1,	KC_F2,		KC_F3,			KC_F13,
-	KC_NO,	KC_NO,			KC_TRNS,		KC_NO,	KC_NO,		    KC_TRNS,    KC_NO,	KC_TRNS,	KC_NO,	        KC_NO
+[_SHORT] = LAYOUT( //TODO: Write Macros for Info, Detail, and Search
+	KC_F1,	KC_NO,		        KC_F13,			KC_NO,	    KC_F15,		KC_F17,	        LSFT_T(KC_INS),		KC_NO,		KC_NO,	    KC_NO,
+	KC_NO,	LSG_T(KC_S),	    KC_DEL,			KC_BSPC,	KC_NO,		KC_MPRV,	    KC_MPLY,	        KC_MNXT,	KC_NO,	    KC_NO,
+	KC_NO,	LCTL_T(KC_PSCR),	CW_TOGG,		KC_MENU,    KC_NO,		LGUI_T(KC_L),   KC_NO,		        KC_NO,		KC_NO,	    KC_NO,
+	KC_NO,	KC_NO,		        KC_TRNS,		KC_NO,	    KC_NO,	    KC_NO,	        KC_NO,		        KC_NO,		KC_NO,	    KC_NO
 ),
 
 /* Mouse Layer
@@ -161,6 +160,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *          `-----'   `--------------------'            `--------------------'   `-----'
  * L Roller: Control Mouse Wheel, L Encoder: Mouse Wheel
  */
+[_MOUSE] = LAYOUT(
+	KC_NO,	KC_NO,		KC_NO,			KC_NO,	    KC_NO,		KC_NO,	    KC_NO,		KC_NO,		KC_NO,	    KC_NO,
+	KC_NO,	KC_NO,		KC_NO,			KC_NO,	    KC_NO,		KC_NO,	    KC_NO,		KC_NO,		KC_NO,	    KC_NO,
+	KC_NO,	KC_NO,		KC_NO,			KC_NO,	    KC_NO,		KC_NO,	    KC_NO,		KC_NO,		KC_NO,	    KC_TRNS,
+	KC_NO,	KC_NO,		KC_NO,		    KC_NO,	    KC_NO,	    KC_NO,	    KC_NO,		KC_NO,		KC_NO,	    KC_NO
+),
 };
 // clang-format on
 
@@ -175,8 +180,8 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [_SYM] = {},
     [_NUM] = {},
     [_SHORT] = {},
-    /* [_MOUSE] = {    ENCODER_CCW_CW(LSFT(KC_MS_WH_DOWN), LSFT(KC_MS_WH_UP)),
-                    ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)} */
+    [_MOUSE] = {    ENCODER_CCW_CW(LSFT(KC_MS_WH_DOWN), LSFT(KC_MS_WH_UP)),
+                    ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)}
 };
 
 #endif
--- keyboards/waterfowl/keymaps/Midlight25/rules.mk
+++ keyboards/waterfowl/keymaps/Midlight25/rules.mk
@@ -1,3 +1,4 @@
 ENCODER_MAP_ENABLE = yes
 CONVERT_TO = elite_pi
+CAPS_WORD_ENABLE = yes
 # VIA_ENABLE = yes
